253d0700dd8317ca0e3264a767fa2220d7d55bd4
sannidhi-s <ssalibindla@gmail.com>
2024-05-14 10:24:50 -0400

    updating css and navbar

--- src/pages/home/Navbar.jsx
+++ src/pages/home/Navbar.jsx
@@ -22,15 +22,15 @@ export default function NavBar() {
 
 
   return (
-    <Navbar expand="lg" fixed="top" bg="light">
+    <Navbar className="navbar navbar-expand-lg fixed-top">
       <Container fluid>
-        <Navbar.Brand href="#heroSection">Work Term Report</Navbar.Brand>
+        <Navbar.Brand className="navbar-brand me-auto" href="#heroSection">Work Term Report</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#heroSection">About</Nav.Link>
-            <Nav.Link href="#experience">Experience</Nav.Link>
-            <Nav.Link href="#acknowledgements">Acknowledgements</Nav.Link>
+          <Nav className=" justify-content-end flex-grow-1 pe-3" data-bs-spy="scroll" data-bs-target="#navigation" data-bs-offset="0" tabindex="0">
+            <Nav.Link className="mg-lx-2" href="#heroSection">About</Nav.Link>
+            <Nav.Link className="mg-lx-2" href="#experience">Experience</Nav.Link>
+            <Nav.Link className="mg-lx-2" href="#acknowledgements">Acknowledgements</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
